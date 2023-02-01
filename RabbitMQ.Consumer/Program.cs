e05b31fd472e6292559a41f25283c86a3c4b1ede
s.savas <s.savas@SSAVAS-NB>
2023-02-01 14:50:16 +0300

    Add project files.

---
+++ RabbitMQ.Consumer/Program.cs
@@ -0,0 +1,31 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+//Bağlantı Oluşturma
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://smlmmjnn:TlCwjz1TsWxldEMzXgkm_wNOnS5-0g6g@hawk.rmq.cloudamqp.com/smlmmjnn");
+
+//Bağlantı Aktifleştirme
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+
+//Queue Oluşturma
+
+channel.QueueDeclare(queue: "example-queue", exclusive: false);// Consumer' da kuyruk publisher daki ile birebir aynı yapıladırmada tanımlanmalıdır.
+
+//Queue Mesaj Okuma
+
+
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume(queue: "example-queue", autoAck: false, consumer: consumer);
+consumer.Received += (sender, e) =>
+{
+    //Kuyruğa gelen mesajın işlendiği yerdir.
+    //e.Body : Kuyruktaki mesajın verisini bütünsel olarak getirecektir.
+    //e.Body.Span veya e.Body.ToArray() : Kuyruktaki mesajın byte verisini getirecektir.
+    Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
+};
+Console.Read();
